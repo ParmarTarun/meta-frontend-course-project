2c52fb17302bd3bfb2a7c27b1daeebbd8847a69e
parmartarun <parmartarun.dev@gmail.com>
2024-01-09 23:08:14 -0500

    💄 Add Tailwind CSS

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex bg-primary">
       <img src="/images/Logo.svg" alt="logo" />
       <Nav />
     </header>
